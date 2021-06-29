78fad92e31bf23a7d849d364eb772dc29d7b4bae
panchopuntoexe <72845439+panchopuntoexe@users.noreply.github.com>
2021-06-29 13:13:42 -0500

    Método para la obtencion de valores de empleado
    Formulario de empleado tiene un método obtenerValores que retorna los valores ingresados por el usuario en un array->nombre,cargo,empleado. El id debería autoincrementarse cuando se crea el empleado leyendo del archivo de empleados.txt

--- src/GestionDeEventos/FormularioEmpleados.java
+++ src/GestionDeEventos/FormularioEmpleados.java
@@ -1,5 +1,9 @@
 package GestionDeEventos;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class FormularioEmpleados {
     private String nombre;
     private String cargo;
@@ -10,7 +14,16 @@ public class FormularioEmpleados {
     public boolean notificar(){
         return true;
     }
-    public void obtenerValores(){
 
+    //retorna los valores ingresados en un array
+    public ArrayList obtenerValores(){
+        ArrayList datosLeidos = new ArrayList();
+        System.out.println("Ingrese el nombre del empleado:");
+        Scanner sc = new Scanner(System.in);
+        datosLeidos.add(sc.nextLine());
+        System.out.println("Ingrese el cargo del empleado:");
+        datosLeidos.add(sc.nextLine());
+        datosLeidos.add("Activo");
+        return datosLeidos;
     }
 }
