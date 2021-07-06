0e5d88d4793e55f067fcec5942a92c3b0969e808
David-Morales-M <74932387+David-Morales-M@users.noreply.github.com>
2021-07-06 00:09:02 -0500

    Refactorización Clases Artista y NoArtista :fokiu:

--- src/GestionDeEventos/ListaDeEmpleados.java
+++ src/GestionDeEventos/ListaDeEmpleados.java
@@ -15,7 +15,6 @@ public class ListaDeEmpleados {
         File archivo;
         FileReader fr;
         BufferedReader br;
-
         try {
             // Apertura del fichero
             archivo = new File ("src/GestionDeEventos/empleados.txt");
