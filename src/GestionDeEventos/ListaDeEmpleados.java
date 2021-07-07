136d76b750a384cfff17835ed3ef96a822deac6f
David-Morales-M <74932387+David-Morales-M@users.noreply.github.com>
2021-07-06 21:36:38 -0500

    Refactorización Total de Empleados :pierre:

--- src/GestionDeEventos/ListaDeEmpleados.java
+++ src/GestionDeEventos/ListaDeEmpleados.java
@@ -16,19 +16,26 @@ public class ListaDeEmpleados {
         FileReader fr;
         BufferedReader br;
         try {
-            // Apertura del fichero
-            archivo = new File ("src/GestionDeEventos/empleados.txt");
-            fr = new FileReader (archivo);
-            br = new BufferedReader(fr);
-
-            // Lectura del fichero
-            String linea;
-            while((linea=br.readLine())!=null)
-                System.out.println(linea);
+            leerArchivo();
         }
         catch(Exception e){
             System.out.println("No se pueden mostrar los empleados");
         }
+    }
+
+    private void leerArchivo() throws IOException {
+        BufferedReader br;
+        File archivo;
+        FileReader fr;
+        // Apertura del fichero
+        archivo = new File ("src/GestionDeEventos/empleados.txt");
+        fr = new FileReader (archivo);
+        br = new BufferedReader(fr);
+
+        // Lectura del fichero
+        String linea;
+        while((linea=br.readLine())!=null)
+            System.out.println(linea);
     }
 
 }
